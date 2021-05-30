9f7fee557371a73c2d4bffbf34f50c1af33cf45b
Adi <adithya.sekar@ibm.com>
2021-05-30 12:15:57 -0400

    Grpc Demo App

---
+++ GprcClient/Program.cs
@@ -0,0 +1,40 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcServer;
+using System;
+using System.Threading.Tasks;
+
+namespace GprcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            //var client = new Greeter.GreeterClient(channel);
+            //var input = new HelloRequest { Name = "Adi" };
+            //var reply = await client.SayHelloAsync(input);
+            //Console.WriteLine(reply.Message);
+
+            var customerClient = new Customer.CustomerClient(channel);
+            var clientRequested = new CustomerLookupModel { UserId = 2 };
+            var customer = await customerClient.GetCustomerInfoAsync(clientRequested);
+            Console.WriteLine($"{customer.FirstName} {customer.LastName}");
+
+            Console.WriteLine();
+            Console.WriteLine("New Customer List");
+            Console.WriteLine();
+
+            // Read from a stream
+            using (var call = customerClient.GetNewCustomers(new NewCustomerRequest()))
+            {
+                while (await call.ResponseStream.MoveNext())
+                {
+                    var currentCustomer = call.ResponseStream.Current;
+                    Console.WriteLine($"{currentCustomer.FirstName} {currentCustomer.LastName}: {currentCustomer.EmailAddress}");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
